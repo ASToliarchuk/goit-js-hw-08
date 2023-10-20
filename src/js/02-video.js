2af3b86652da623b4f5f5e24dc28ce85e7a82cd5
ASToliarchuk <stolyarchukast@gmail.com>
2023-10-20 22:29:20 +0200

    02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,7 @@ import Player from '@vimeo/player';
 // Імпорт throttle
 const throttle = require('lodash.throttle');
 
-const CURRENT_TIME = 'videoplayer-current-time';
+const storageKey = 'videoplayer-current-time';
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
@@ -12,12 +12,12 @@ player.on('timeupdate', throttle(timeplaying, 1000));
 
 function timeplaying(event) {
   const currentTime = event.seconds;
-  localStorage.setItem(CURRENT_TIME, JSON.stringify(currentTime));
+  localStorage.setItem(storageKey, JSON.stringify(currentTime));
 
   console.log(`Час перегляду ${currentTime} секунд`);
 }
 // player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME)));
-const timeLS = localStorage.getItem(CURRENT_TIME);
+const timeLS = localStorage.getItem(storageKey);
 if (timeLS) {
   const savedTime = JSON.parse(timeLS);
   if (savedTime) {
