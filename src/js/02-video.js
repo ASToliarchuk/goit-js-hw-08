fe821cde7e2621905434d4a7e8cb2d78a2df9634
ASToliarchuk <stolyarchukast@gmail.com>
2023-10-20 22:24:13 +0200

    tack2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+// Імпорт бібліотеки
+import Player from '@vimeo/player';
 
+// Імпорт throttle
+const throttle = require('lodash.throttle');
+
+const CURRENT_TIME = 'videoplayer-current-time';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(timeplaying, 1000));
+
+function timeplaying(event) {
+  const currentTime = event.seconds;
+  localStorage.setItem(CURRENT_TIME, JSON.stringify(currentTime));
+
+  console.log(`Час перегляду ${currentTime} секунд`);
+}
+// player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_TIME)));
+const timeLS = localStorage.getItem(CURRENT_TIME);
+if (timeLS) {
+  const savedTime = JSON.parse(timeLS);
+  if (savedTime) {
+    player.setCurrentTime(savedTime);
+    console.log(`Ви закічили перегляд на ${Math.round(savedTime)} секунді`);
+  }
+} else {
+  console.log(`Ви не переглядали це відео`);
+}
